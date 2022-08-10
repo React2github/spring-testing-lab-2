e0354fb3fa63133287fafa4cdd7f766bda70a95b
Abdella <abdella.shukur2@gmail.com>
2022-08-10 13:03:55 -0400

    bitcoin price returning

---
+++ Springlab2/src/main/java/com/example/Springlab2/Controller/BitcoinDataController.java
@@ -0,0 +1,25 @@
+package com.example.Springlab2.Controller;
+
+import com.example.Springlab2.Service.BitCoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BitcoinDataController {
+
+@Autowired
+private BitCoinService bitcoinService;
+
+public BitcoinDataController(BitCoinService bitcoinService) {
+
+    this.bitcoinService = bitcoinService;
+}
+
+    @GetMapping("/bitcoin")
+    public String getPrice() {
+    String response = "The price of one bitcoin is " + Math.round(bitcoinService.getBitcoinPrice());
+ return response;
+    }
+
+}
---
+++ Springlab2/src/main/java/com/example/Springlab2/Springlab2Application.java
@@ -0,0 +1,13 @@
+package com.example.Springlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlab2Application.class, args);
+	}
+
+}
